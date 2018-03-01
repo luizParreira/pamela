848e7724dd3bd9070cc7f9ea4ca908965318ef0a
Luiz Parreira <luizfilipester@gmail.com>
2018-03-01 10:39:00 -0300

    Improvode config and removed unused declarations

--- lib/pamela_web/endpoint.ex
+++ lib/pamela_web/endpoint.ex
@@ -5,36 +5,37 @@ defmodule PamelaWeb.Endpoint do
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :pamela, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug Phoenix.CodeReloader
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.RequestId
-  plug Plug.Logger
+  plug(Plug.RequestId)
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
+  plug(
+    Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Poison
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
+  plug(
+    Plug.Session,
     store: :cookie,
     key: "_pamela_key",
     signing_salt: "57UIQC8C"
+  )
 
-  plug PamelaWeb.Router
+  plug(PamelaWeb.Router)
 
   @doc """
   Callback invoked for dynamically configuring the endpoint.
