845d356690a0ccd13704a4411b091dd4b676c7fa
Luiz Parreira <luizfilipester@gmail.com>
2018-03-01 11:24:19 -0300

    Remove cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,6 @@ config :pamela, PamelaWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "pamela-bot.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :pamela, Pamela.Repo,
