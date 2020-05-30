5532c96513675003bd330740d4cbb5a18b786342
Luiz Parreira <luizfilipester@gmail.com>
2020-05-30 11:08:56 -0300

    Fix user test

--- test/pamela/user/user_test.exs
+++ test/pamela/user/user_test.exs
@@ -6,8 +6,18 @@ defmodule Pamela.UserTest do
   describe "telegram_users" do
     alias Pamela.User.TelegramUser
 
-    @valid_attrs %{first_name: "some first_name", id: 42, last_name: "some last_name", username: "some username"}
-    @update_attrs %{first_name: "some updated first_name", id: 43, last_name: "some updated last_name", username: "some updated username"}
+    @valid_attrs %{
+      first_name: "some first_name",
+      id: 42,
+      last_name: "some last_name",
+      username: "some username"
+    }
+    @update_attrs %{
+      first_name: "some updated first_name",
+      id: 43,
+      last_name: "some updated last_name",
+      username: "some updated username"
+    }
     @invalid_attrs %{first_name: nil, id: nil, last_name: nil, username: nil}
 
     def telegram_user_fixture(attrs \\ %{}) do
@@ -24,9 +34,9 @@ defmodule Pamela.UserTest do
       assert User.list_telegram_users() == [telegram_user]
     end
 
-    test "get_telegram_user!/1 returns the telegram_user with given id" do
+    test "get_telegram_user/1 returns the telegram_user with given id" do
       telegram_user = telegram_user_fixture()
-      assert User.get_telegram_user!(telegram_user.id) == telegram_user
+      assert User.get_telegram_user(telegram_user.id) == telegram_user
     end
 
     test "create_telegram_user/1 with valid data creates a telegram_user" do
@@ -53,14 +63,17 @@ defmodule Pamela.UserTest do
 
     test "update_telegram_user/2 with invalid data returns error changeset" do
       telegram_user = telegram_user_fixture()
-      assert {:error, %Ecto.Changeset{}} = User.update_telegram_user(telegram_user, @invalid_attrs)
-      assert telegram_user == User.get_telegram_user!(telegram_user.id)
+
+      assert {:error, %Ecto.Changeset{}} =
+               User.update_telegram_user(telegram_user, @invalid_attrs)
+
+      assert telegram_user == User.get_telegram_user(telegram_user.id)
     end
 
     test "delete_telegram_user/1 deletes the telegram_user" do
       telegram_user = telegram_user_fixture()
       assert {:ok, %TelegramUser{}} = User.delete_telegram_user(telegram_user)
-      assert_raise Ecto.NoResultsError, fn -> User.get_telegram_user!(telegram_user.id) end
+      assert User.get_telegram_user(telegram_user.id) == nil
     end
 
     test "change_telegram_user/1 returns a telegram_user changeset" do
